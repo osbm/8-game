4274cdc24868fed33b7147a29128ee51809b242f
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2021-11-26 16:09:17 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std; // uwu
+
+
+void print (string context) {
+    cout << context << endl;
+}
+
+class Graph
+{    
+public:
+    string board;
+    int visitedNodes = 0;
+    
+    int * get_child_vertices (int board){
+        return {0};
+    }
+};
+
+
+
+
+int main() {
+    // Add the Graph object
+    // add the BFS search
+    int biggest = 1000000000;
+    cout << biggest << endl;
+    return 0;
+}
